96d019e9d23c4c332ee172d25cb726cd30275634
smarsh25 <sjm.atl@gmail.com>
2014-01-28 22:30:04 -0800

    adding first routes and views for products. basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,31 @@
 Store::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # root 'categories#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # get '/categories', to: 'categories#index', as: categories
+  # get '/categories/:id', to: 'categories#show', as: category
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  root 'products#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get '/products', to: 'products#index', as: 'products'
+  get '/products/:id', to: 'products#show', as: 'product'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
+
+
+# class CategoriesController < ApplicationController
+
+#   def index
+#     @categories = Category.all
+#   end
+
+#   def show
+#   end
+
+# end
+
+# <h2>Categories</h2>
+# <ul>
+#   <% @categories.each do |c| %>
+#     <li><%= link_to(c.name, category_path(c)) %></li>
+#   <% end %>
+# </ul>
