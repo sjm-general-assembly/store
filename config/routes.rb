5356f5166126199e64b00c8aadc032c2f16889b3
smarsh25 <sjm.atl@gmail.com>
2014-01-28 22:36:06 -0800

    adding first routes and views for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,12 @@
 Store::Application.routes.draw do
-  # root 'categories#index'
+  root 'categories#index'
 
-  # get '/categories', to: 'categories#index', as: categories
-  # get '/categories/:id', to: 'categories#show', as: category
+  get '/categories', to: 'categories#index', as: 'categories'
+  get '/categories/:id', to: 'categories#show', as: 'category'
 
-  root 'products#index'
+  # root 'products#index'
 
   get '/products', to: 'products#index', as: 'products'
   get '/products/:id', to: 'products#show', as: 'product'
 
 end
-
-
-# class CategoriesController < ApplicationController
-
-#   def index
-#     @categories = Category.all
-#   end
-
-#   def show
-#   end
-
-# end
-
-# <h2>Categories</h2>
-# <ul>
-#   <% @categories.each do |c| %>
-#     <li><%= link_to(c.name, category_path(c)) %></li>
-#   <% end %>
-# </ul>
